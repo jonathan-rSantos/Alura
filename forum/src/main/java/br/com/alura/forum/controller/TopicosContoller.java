1e508e468bc05d131f6d2ff393777ed86612751a
Jonathan <jonathan.ribeiro28121997@gmail.com>
2023-03-18 00:07:03 -0300

    Metodo Put Topico

--- forum/src/main/java/br/com/alura/forum/controller/TopicosContoller.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosContoller.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,17 +17,49 @@ import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
-//@RequestMapping("/topicos")
+@RequestMapping("/topicos")
 public class TopicosContoller {
 
 	@Autowired
 	private TopicoRepository topicoRepository;
 
-	// @GetMapping
-	@RequestMapping("/topicos")
+	@GetMapping
 	public List<TopicoDto> lista() {
 		List<Topico> topicos = topicoRepository.findAll();
 		return TopicoDto.converter(topicos);
 	}
+	
+	//Detalhando um topico
+	//@RequestMapping("/topico")
+	//public List<TopicoDto> listaTopico(String nomeCurso){
+		//List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+		//return TopicoDto.converter(topicos);
+	//}
+	
+	@PutMapping
+	@RequestBody
+	public void cadastrar () {
+		
+	}
+	
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
